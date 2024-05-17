b9274f5d0d633bb61db214e612f5458f6391732e
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2024-05-17 09:09:52 -0500

    Update models.py

--- Engine/models.py
+++ Engine/models.py
@@ -1,12 +1,13 @@
 from app import db
-from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-# Define User model
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    role = db.Column(db.String(80), nullable=False)
+    username = db.Column(db.String(64), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
