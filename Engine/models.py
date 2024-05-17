6883fd8d0fc1f9da7752d448fb1420ac41ff872f
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2024-05-17 09:14:54 -0500

    Update models.py

--- Engine/models.py
+++ Engine/models.py
@@ -5,7 +5,6 @@ from flask_login import UserMixin
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
     role = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
