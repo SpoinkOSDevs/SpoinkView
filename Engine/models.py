d5e5d99f8126f0cb0ba5259894ba4664ff93f1b8
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2024-05-17 09:18:28 -0500

    Update models.py

--- Engine/models.py
+++ Engine/models.py
@@ -6,7 +6,9 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
+    role = db.Column(db.String(64), nullable=False, default='User')  # Assuming default role is 'User'
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
