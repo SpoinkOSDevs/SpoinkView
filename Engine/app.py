2a8fd8fe52bfc32583882b13fc07703822e76c8f
SpoinkOSDevs <SpoinkOSGithub@gmail.com>
2024-05-17 09:25:27 -0500

    Update app.py

--- Engine/app.py
+++ Engine/app.py
@@ -1,31 +1,17 @@
-from flask import Flask, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
-from models import User
-from routes import *
+from flask import Flask
+from models import db, User
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your_secret_key'
+
+# Load configuration from the models file
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable tracking modifications to improve performance
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
+# Initialize the SQLAlchemy object with the Flask app
+db.init_app(app)
 
-# Check if an admin account exists in the database
-def admin_account_exists():
-    return User.query.filter_by(role='Admin').first() is not None
-
-# Route for root URL
-@app.route('/')
-def index():
-    if not admin_account_exists():
-        return redirect(url_for('setup_admin'))
-    else:
-        return redirect(url_for('login'))
+# Import routes after initializing app and db to avoid circular imports
+from routes import *
 
 if __name__ == '__main__':
     app.run(debug=True)
